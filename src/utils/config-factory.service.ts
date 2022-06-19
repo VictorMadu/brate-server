1ebae3a83b9a6cf6837048c5cc77c49fc472472e
VictorMadu <ebubevm@gmail.com>
2022-06-19 11:05:28 +0100

    fix: compile error

--- src/utils/config-factory.service.ts
+++ src/utils/config-factory.service.ts
@@ -1,35 +1,35 @@
-import { Injectable } from 'nist-core/injectables';
-import { InnerKeys, InnerValue } from 'ts-util-types';
+import { Injectable } from "nist-core/injectables";
+import { InnerKeys, InnerValue } from "ts-util-types";
 import {
-    ConfigDevManager,
-    ConfigManger,
-    ConfigProdManager,
-    ConfigTestManager,
-} from './config-manager';
-import { Config } from './interfaces/config-manager.interface';
+  ConfigDevManager,
+  ConfigManger,
+  ConfigProdManager,
+  ConfigTestManager,
+} from "./config-manager";
+import { Config } from "./interfaces/config-manager.interface";
 
 @Injectable()
 export class ConfigService {
-    AppConfigGetter: ConfigManger<Config>;
+  AppConfigGetter: ConfigManger<Config>;
 
-    constructor() {
-        this.AppConfigGetter = this.createAppConfigGetter();
-    }
+  constructor() {
+    this.AppConfigGetter = this.createConfigGetter();
+  }
 
-    public get<K extends InnerKeys<Config>>(key: K): InnerValue<Config, K> {
-        return this.AppConfigGetter.get(key);
-    }
+  public get<K extends InnerKeys<Config>>(key: K): InnerValue<Config, K> {
+    return this.AppConfigGetter.get(key);
+  }
 
-    private createConfigGetter() {
-        switch (process.env.NODE_ENV) {
-            case 'development':
-                return new ConfigDevManager<Config>();
-            case 'test':
-                return new ConfigTestManager<Config>();
-            case 'production':
-                return new ConfigProdManager<Config>();
-            default:
-                throw new Error('Unsupported NODE_ENV');
-        }
+  private createConfigGetter() {
+    switch (process.env.NODE_ENV) {
+      case "development":
+        return new ConfigDevManager<Config>();
+      case "test":
+        return new ConfigTestManager<Config>();
+      case "production":
+        return new ConfigProdManager<Config>();
+      default:
+        throw new Error("Unsupported NODE_ENV");
     }
+  }
 }
