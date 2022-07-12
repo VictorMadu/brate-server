769baf65d13ab43204f4bacd00d5c8fee20bb253
VictorMadu <ebubevm@gmail.com>
2022-07-12 20:38:40 +0100

    feat: Change config structure

--- src/utils/interfaces/config-manager.interface.ts
+++ src/utils/interfaces/config-manager.interface.ts
@@ -1,32 +1,34 @@
 // TODO: Add the commit and line ref to this in the documentation of the fix for `InnerKeys` in `ts-util-types`
 
 export interface Config {
-  app: {
-    address: string;
-    port: number;
-  };
-  jwt: {
-    secretKey: string;
-  };
-  postgres: {
-    host: string;
-    port: number;
-    database: string;
-    clients: {
-      currency: {
-        username: string;
-        password: string;
-        pool_size: number;
-      };
+    app: {
+        host: string;
+        port: number;
+        cors: string[];
+        https: Record<"keyFilePath" | "certFilePath", string> | null;
     };
-  };
-  bcrypt: {
-    saltRounds: number;
-    expiryAfter: number;
-  };
+    jwt: {
+        secretKey: string;
+    };
+    postgres: {
+        host: string;
+        port: number;
+        database: string;
+        clients: {
+            currency: {
+                username: string;
+                password: string;
+                pool_size: number;
+            };
+        };
+    };
+    bcrypt: {
+        saltRounds: number;
+        expiryAfter: number;
+    };
 
-  currency_rate: {
-    url: string;
-    api_keys: string[];
-  };
+    currency_rate: {
+        url: string;
+        api_keys: string[];
+    };
 }
