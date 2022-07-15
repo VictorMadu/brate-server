49ce53ab4b221d8c2164444dbf7698fa82a8cd51
VictorMadu <ebubevm@gmail.com>
2022-07-15 16:02:10 +0100

    chore: database set-up and clean-up to use when testing delete-notification-list

---
+++ src/user/delete-notification-list/clean-up-set-up-query.sql
@@ -0,0 +1,28 @@
+
+-- Clean up function after test
+DROP FUNCTION IF EXISTS clean_delete_notification_list_test_set_up;
+
+CREATE FUNCTION clean_delete_notification_list_test_set_up() RETURNS VOID 
+AS $$
+DECLARE
+    _user_id uuid = 'e027bbec-bfb6-493f-b219-741bd86a126b';
+BEGIN
+    DELETE FROM notifications n
+    USING users u
+    WHERE u.user_id = _user_id AND u.user_id = n.user_id;
+
+    DELETE FROM web_clients w 
+    USING users u 
+    WHERE u.user_id = _user_id;
+
+    DELETE FROM user_verification_details v
+    USING users u
+    WHERE u.user_id = _user_id AND v.user_verification_details_id = ANY(u.verification_details); 
+
+    DELETE FROM users 
+    WHERE users.user_id = _user_id;
+RETURN;
+END;
+$$ LANGUAGE PLPGSQL;
+
+SELECT clean_delete_notification_list_test_set_up();
