25df25d0a9306a49d831ac2e038239c4300679ba
VictorMadu <ebubevm@gmail.com>
2022-07-15 11:19:59 +0100

    refactor: Remvoe duplicate of get-notification-list

--- src/user/get-notifications-list/interface/index.ts
+++
@@ -1,8 +0,0 @@
-import { headerSchema } from "../schema/header.schema";
-import { querystringSchema } from "../schema/querystring.schema";
-import { responseSchema } from "../schema/response.schema";
-import { FromSchema } from "json-schema-to-ts";
-
-export type Headers = FromSchema<typeof headerSchema>;
-export type Query = FromSchema<typeof querystringSchema>;
-export type Response = FromSchema<typeof responseSchema>;
