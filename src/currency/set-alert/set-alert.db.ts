da06bf7261c4b83cdc02330d398eaad57c701b5a
VictorMadu <ebubevm@gmail.com>
2022-07-16 10:27:07 +0100

    refactor: Improve set-alert code quality
    Also works on the postman documentation and adds database set-up and clean-up to use

--- src/currency/set-alert/set-alert.db.ts
+++ src/currency/set-alert/set-alert.db.ts
@@ -1,99 +1,87 @@
-import { Injectable } from "nist-core/injectables";
+import { Injectable } from "victormadu-nist-core";
 import { CurrencyPostgresDbService as PostgresDbService } from "../_utils/currency.db.service";
 import { PoolClient, QueryResult } from "pg";
 import {
-  wallet_currency_transactions as transactions,
-  sellers,
-  blackRates,
-  price_alerts,
-  parallelRates,
-} from "../../../utils/postgres-db-types/erate";
-import { PostgresHeplper, PostgresPoolClientRunner } from "../../../utils/postgres-helper";
+    wallet_currency_transactions as transactions,
+    sellers,
+    blackRates,
+    price_alerts,
+    parallelRates,
+} from "../../utils/postgres-db-types/erate";
+import { PostgresHeplper, PostgresPoolClientRunner } from "../../utils/postgres-helper";
 
 interface InData {
-  userId: string;
-  targetRate: number;
-  marketType: "parallel" | "black";
-  baseCurrency: string;
-  quotaCurrency: string;
+    userId: string;
+    targetRate: number;
+    marketType: "parallel" | "black";
+    baseCurrency: string;
+    quotaCurrency: string;
 }
 
 @Injectable()
 export class DbService {
-  constructor(
-    private currencyDb: PostgresDbService,
-    private helper: PostgresHeplper,
-    private runner: PostgresPoolClientRunner
-  ) {}
+    constructor(
+        private currencyDb: PostgresDbService,
+        private helper: PostgresHeplper,
+        private runner: PostgresPoolClientRunner
+    ) {}
 
-  private onReady() {
-    this.runner.setPsql(this.currencyDb.getPsql());
-  }
-
-  async setAlert(inData: InData): Promise<boolean> {
-    return (
-      (await this.runner.runQuery(async (psql) => {
-        const isSuccessful = await this._setAlert(psql, inData);
-        if (!isSuccessful) return undefined;
-        return true;
-      })) || false
-    );
-  }
-
-  // TODO: use pl/psSQL
-  // buyer gives base to seller
-  // seller gives quota to buyer
-  async _setAlert(psql: PoolClient, inData: InData): Promise<boolean> {
-    const queryCreator = new SetAlertQueryCreator(
-      {
-        userId: this.helper.sanitize(inData.userId),
-        targetRate: this.helper.sanitize(inData.targetRate),
-        baseCurrency: this.helper.sanitize(inData.baseCurrency),
-        quotaCurrency: this.helper.sanitize(inData.quotaCurrency),
-      },
-      {
-        marketType: inData.marketType,
-      }
-    );
-
-    console.log("_setAlert query", queryCreator.getQuery());
-    try {
-      const result = await psql.query(queryCreator.getQuery());
-      console.log("_setAlert result", result);
-      return this.helper.hasAlteredTable(result);
-    } catch (error) {
-      console.log("_setAlert error", error);
-      throw error;
+    private onReady() {
+        this.runner.setPsql(this.currencyDb.getPsql());
     }
-  }
+
+    async setAlert(inData: InData): Promise<boolean> {
+        return !!(await this.runner.runQuery((psql) => this._setAlert(psql, inData)));
+    }
+
+    // TODO: use pl/psSQL
+    // buyer gives base to seller
+    // seller gives quota to buyer
+    async _setAlert(psql: PoolClient, inData: InData): Promise<boolean> {
+        const queryCreator = new SetAlertQueryCreator(
+            {
+                userId: this.helper.sanitize(inData.userId),
+                targetRate: this.helper.sanitize(inData.targetRate),
+                baseCurrency: this.helper.sanitize(inData.baseCurrency),
+                quotaCurrency: this.helper.sanitize(inData.quotaCurrency),
+            },
+            {
+                marketType: inData.marketType,
+            }
+        );
+
+        console.log("_setAlert query", queryCreator.getQuery());
+        const result = await psql.query(queryCreator.getQuery());
+        return this.helper.hasAlteredTable(result);
+    }
 }
 
 // TODO: Rewrite this using state pattern
 class SetAlertQueryCreator {
-  private userId: string;
-  private targetRate: number;
-  private marketType: "parallel" | "black";
-  private baseCurrency: string;
-  private quotaCurrency: string;
+    private userId: string;
+    private targetRate: number;
+    private marketType: "parallel" | "black";
+    private baseCurrency: string;
+    private quotaCurrency: string;
 
-  constructor(
-    sanitizedInData: {
-      userId: string;
-      targetRate: number;
-      baseCurrency: string;
-      quotaCurrency: string;
-    },
-    config: { marketType: "parallel" | "black" }
-  ) {
-    this.userId = sanitizedInData.userId;
-    this.targetRate = sanitizedInData.targetRate;
-    this.baseCurrency = sanitizedInData.baseCurrency;
-    this.quotaCurrency = sanitizedInData.quotaCurrency;
-    this.marketType = config.marketType;
-  }
+    constructor(
+        sanitizedInData: {
+            userId: string;
+            targetRate: number;
+            baseCurrency: string;
+            quotaCurrency: string;
+        },
+        config: { marketType: "parallel" | "black" }
+    ) {
+        this.userId = sanitizedInData.userId;
+        this.targetRate = sanitizedInData.targetRate;
+        this.baseCurrency = sanitizedInData.baseCurrency;
+        this.quotaCurrency = sanitizedInData.quotaCurrency;
+        this.marketType = config.marketType;
+    }
 
-  getQuery() {
-    return `
+    getQuery() {
+        return `
       INSERT INTO 
         ${price_alerts.$$NAME}
         (
@@ -127,21 +115,21 @@ class SetAlertQueryCreator {
           FETCH FIRST ROW ONLY
         )
     `;
-  }
+    }
 
-  private createMarketTypeDbType() {
-    if (this.marketType === "black") return `'B'`;
-    return `'P'`;
-  }
+    private createMarketTypeDbType() {
+        if (this.marketType === "black") return `'B'`;
+        return `'P'`;
+    }
 
-  private createGetMarketAvgRateQuery() {
-    if (this.marketType === "black") return this.createGetBlackMarketAvgRateQuery();
-
-    return this.createGetParallelMarketAvgRateQuery();
-  }
-  private createGetBlackMarketAvgRateQuery() {
-    const t = "__t";
-    return `
+    // TODO: Split this function, create a factory class for getting the average rate query for the market type, the implementation to be returned by the factory should implement a getAvgRateQuery()
+    private createGetMarketAvgRateQuery() {
+        if (this.marketType === "black") return this.createGetBlackMarketAvgRateQuery();
+        return this.createGetParallelMarketAvgRateQuery();
+    }
+    private createGetBlackMarketAvgRateQuery() {
+        const t = "__t";
+        return `
       SELECT 
         avg(${t}.${blackRates.rate})
       FROM 
@@ -160,17 +148,17 @@ class SetAlertQueryCreator {
       WHERE 
         ${t}.${blackRates.rate} IS NOT NULL
     `;
-  }
+    }
 
-  private createGetParallelMarketAvgRateQuery() {
-    return `SELECT
+    private createGetParallelMarketAvgRateQuery() {
+        return `SELECT
       (${this.createGetLastestParallelRateOfCurrency(
-        this.quotaCurrency
+          this.quotaCurrency
       )})/(${this.createGetLastestParallelRateOfCurrency(this.baseCurrency)})`;
-  }
+    }
 
-  private createGetLastestParallelRateOfCurrency(currencyId: string) {
-    return `
+    private createGetLastestParallelRateOfCurrency(currencyId: string) {
+        return `
       SELECT  
         ${parallelRates.rate} 
       FROM
@@ -182,5 +170,5 @@ class SetAlertQueryCreator {
         ${parallelRates.time} DESC
       FETCH FIRST ROW ONLY
     `;
-  }
+    }
 }
--- src/currency/set-alert/set-alert.http.controller.ts
+++ src/currency/set-alert/set-alert.http.controller.ts
@@ -1,9 +1,7 @@
-import { HttpController } from "nist-fastify-adapter/injectables/http-controller";
-import * as HttpMethods from "nist-fastify-adapter/injectables/http.method.decorators";
-import * as HttpParams from "nist-fastify-adapter/injectables/http.param.decorators";
+import { HttpController, Schema, Send, Body, Headers, Put } from "victormadu-nist-fastify-adapter";
 import { Service } from "./set-alert.service";
 
-import { Headers, Body, ResXXX } from "./interface";
+import { Headers as ReqHeaders, Body as ReqBody, ResXXX } from "./interface";
 import { headerSchema } from "./schema/header.schema";
 import { bodySchema } from "./schema/body.schema";
 import { resxxSchema } from "./schema/response.schema";
@@ -14,32 +12,28 @@ const DEFAULT_MARKET_TYPE = "parallel";
 
 @HttpController(ALERT)
 export class Controller {
-  constructor(private service: Service) {}
+    constructor(private service: Service) {}
 
-  @HttpMethods.Schema({
-    headers: headerSchema,
-    body: bodySchema,
-    // response: {
-    //   xxx: resxxSchema,
-    // },
-  })
-  @HttpMethods.Put()
-  async route(
-    @HttpParams.Headers() headers: Headers,
-    @HttpParams.Body() body: Body,
-    @HttpParams.Send() send: ResSend
-  ) {
-    const [code, msg] = await this.service.handle({
-      authToken: headers.authorization,
-      targetRate: body.target_rate,
-      marketType: body.market_type ?? DEFAULT_MARKET_TYPE,
-      baseCurrency: body.base,
-      quotaCurrency: body.quota,
-    });
+    @Schema({
+        headers: headerSchema,
+        body: bodySchema,
+        // response: {
+        //   xxx: resxxSchema,
+        // },
+    })
+    @Put()
+    async route(@Headers() headers: ReqHeaders, @Body() body: ReqBody, @Send() send: ResSend) {
+        const [code, msg] = await this.service.handle({
+            authToken: headers.authorization,
+            targetRate: body.target_rate,
+            marketType: body.market_type ?? DEFAULT_MARKET_TYPE,
+            baseCurrency: body.base.toUpperCase(),
+            quotaCurrency: body.quota.toUpperCase(),
+        });
 
-    send<ResXXX>(code, {
-      status: code < 300,
-      msg,
-    });
-  }
+        send<ResXXX>(code, {
+            status: code < 300,
+            msg,
+        });
+    }
 }
