e6853bb58dc60f21c948fece668685682554a723
VictorMadu <ebubevm@gmail.com>
2022-07-18 02:10:35 +0100

    refactor: Improve unstar-favourite code quality
    Also works on the postman documentation and adds database set-up and clean-up to use

--- src/currency/unstar-favourite/unstar-favourite.db.ts
+++ src/currency/unstar-favourite/unstar-favourite.db.ts
@@ -1,56 +1,55 @@
-import { Injectable } from "nist-core/injectables";
+import { Injectable } from "victormadu-nist-core";
 import { CurrencyPostgresDbService } from "../_utils/currency.db.service";
 import { PoolClient } from "pg";
-import { user_favourite_currency_pairs } from "../../../utils/postgres-db-types/erate";
-import { PostgresHeplper, PostgresPoolClientRunner } from "../../../utils/postgres-helper";
+import { user_favourite_currency_pairs } from "../../utils/postgres-db-types/erate";
+import { PostgresHeplper, PostgresPoolClientRunner } from "../../utils/postgres-helper";
 
 const table = user_favourite_currency_pairs;
 
 interface InData {
-  userId: string;
-  base: string;
-  quota: string;
+    userId: string;
+    base: string;
+    quota: string;
 }
 
 @Injectable()
 export class DbService {
-  constructor(
-    private currencyDb: CurrencyPostgresDbService,
-    private helper: PostgresHeplper,
-    private runner: PostgresPoolClientRunner
-  ) {}
+    constructor(
+        private currencyDb: CurrencyPostgresDbService,
+        private helper: PostgresHeplper,
+        private runner: PostgresPoolClientRunner
+    ) {}
 
-  private onReady() {
-    this.runner.setPsql(this.currencyDb.getPsql());
-  }
+    private onReady() {
+        this.runner.setPsql(this.currencyDb.getPsql());
+    }
 
-  async removeToFavourite(inData: InData): Promise<number> {
-    return (
-      (await this.runner.runQuery(
-        async (psql) => await this.addCurrTimeToDeletedField(psql, inData)
-      )) ?? 0
-    );
-  }
+    async removeToFavourite(inData: InData): Promise<number> {
+        return (
+            (await this.runner.runQuery((psql) => this.addCurrTimeToDeletedField(psql, inData))) ??
+            0
+        );
+    }
 
-  async addCurrTimeToDeletedField(psql: PoolClient, inData: InData) {
-    const queryCreator = new AddCurrTimeToDeletedFieldQueryCreator(inData, this.helper);
-    const result = await psql.query(queryCreator.getQuery());
-    return this.helper.getAlteredRowLenth(result);
-  }
+    async addCurrTimeToDeletedField(psql: PoolClient, inData: InData) {
+        const queryCreator = new AddCurrTimeToDeletedFieldQueryCreator(inData, this.helper);
+        const result = await psql.query(queryCreator.getQuery());
+        return this.helper.getAlteredRowLenth(result);
+    }
 }
 
 class AddCurrTimeToDeletedFieldQueryCreator {
-  private userId: string;
-  private base: string;
-  private quota: string;
-  constructor(inData: InData, sanitizer: { sanitize: (obj: any) => any }) {
-    this.userId = sanitizer.sanitize(inData.userId);
-    this.base = sanitizer.sanitize(inData.base);
-    this.quota = sanitizer.sanitize(inData.quota);
-  }
+    private userId: string;
+    private base: string;
+    private quota: string;
+    constructor(inData: InData, sanitizer: { sanitize: (obj: any) => any }) {
+        this.userId = sanitizer.sanitize(inData.userId);
+        this.base = sanitizer.sanitize(inData.base);
+        this.quota = sanitizer.sanitize(inData.quota);
+    }
 
-  getQuery() {
-    return `
+    getQuery() {
+        return `
     UPDATE
       ${table.$$NAME}
     SET
@@ -61,29 +60,29 @@ class AddCurrTimeToDeletedFieldQueryCreator {
       ${table.quota} = ${this.quota} AND
       (
         ${this.getLastItemFromArr(
-          this.coalesceTodefaultArr(table.unfavourite_at, "TIMESTAMPTZ[]")
+            this.coalesceTodefaultArr(table.unfavourite_at, "TIMESTAMPTZ[]")
         )} IS NULL OR 
         ${this.getLastItemFromArr(
-          this.coalesceTodefaultArr(table.favourite_at, "TIMESTAMPTZ[]")
+            this.coalesceTodefaultArr(table.favourite_at, "TIMESTAMPTZ[]")
         )} >= 
         ${this.getLastItemFromArr(this.coalesceTodefaultArr(table.unfavourite_at, "TIMESTAMPTZ[]"))}
       )
     `;
-  }
+    }
 
-  private getLastItemFromArr(arrQuery: string) {
-    const _arrQuery = `(${arrQuery})`;
-    return `${this.getNthItemFromArr(_arrQuery, `array_length(${_arrQuery},1)`)}`;
-  }
+    private getLastItemFromArr(arrQuery: string) {
+        const _arrQuery = `(${arrQuery})`;
+        return `${this.getNthItemFromArr(_arrQuery, `array_length(${_arrQuery},1)`)}`;
+    }
 
-  private getNthItemFromArr(arrQuery: string, nthQuery: string) {
-    const _arrQuery = `(${arrQuery})`;
-    const _nthQuery = `(${nthQuery})`;
-    return `${_arrQuery}[${_nthQuery}]`;
-  }
+    private getNthItemFromArr(arrQuery: string, nthQuery: string) {
+        const _arrQuery = `(${arrQuery})`;
+        const _nthQuery = `(${nthQuery})`;
+        return `${_arrQuery}[${_nthQuery}]`;
+    }
 
-  private coalesceTodefaultArr(arrQuery: string, datatype: string) {
-    const _arrQuery = `(${arrQuery})`;
-    return `COALESCE(${_arrQuery}, ARRAY[]::${datatype})`;
-  }
+    private coalesceTodefaultArr(arrQuery: string, datatype: string) {
+        const _arrQuery = `(${arrQuery})`;
+        return `COALESCE(${_arrQuery}, ARRAY[]::${datatype})`;
+    }
 }
--- src/currency/unstar-favourite/unstar-favourite.service.ts
+++ src/currency/unstar-favourite/unstar-favourite.service.ts
@@ -1,46 +1,44 @@
-import { Injectable } from "nist-core/injectables";
-import { AuthManagerService } from "../../../utils/auth-manager.service";
+import { Injectable } from "victormadu-nist-core";
+import { AuthManagerService } from "../../utils/auth-manager.service";
 import { DbService } from "./unstar-favourite.db";
 
 interface ServiceInData {
-  authToken: string;
-  currency_pairs: string[];
+    authToken: string;
+    currency_pairs: string[];
 }
 
 interface OutData {
-  deleted_count: number;
+    deleted_count: number;
 }
 
 @Injectable()
 export class Service {
-  constructor(private dbService: DbService, private authManager: AuthManagerService) {}
+    constructor(private dbService: DbService, private authManager: AuthManagerService) {}
 
-  async handle(inData: ServiceInData): Promise<[number, string, OutData | undefined]> {
-    const userId = this.authManager.parse(inData.authToken).userId;
-    if (!userId) return [401, "", undefined];
+    async handle(inData: ServiceInData): Promise<[number, string, OutData | undefined]> {
+        const userId = this.authManager.parse(inData.authToken).userId;
+        if (!userId) return [401, "", undefined];
 
-    return await this.removeAllToFavourite(userId, inData);
-  }
-
-  async removeAllToFavourite(
-    userId: string,
-    inData: ServiceInData
-  ): Promise<[number, string, OutData | undefined]> {
-    const uniqueCurrencyPairs = [...new Set(inData.currency_pairs)];
-    const len = uniqueCurrencyPairs.length;
-    let deletedCount = 0;
-    let i = 0;
-    while (i < len) {
-      const [base, quota] = uniqueCurrencyPairs[i].split(" ");
-      i++;
-      if (base === quota || base == null) continue;
-      deletedCount += await this.dbService.removeToFavourite({
-        userId,
-        base,
-        quota,
-      });
+        return await this.removeAllToFavourite(userId, inData);
     }
 
-    return [200, "Successful", { deleted_count: deletedCount }];
-  }
+    async removeAllToFavourite(
+        userId: string,
+        inData: ServiceInData
+    ): Promise<[number, string, OutData | undefined]> {
+        const uniqueCurrencyPairs = [...new Set(inData.currency_pairs)];
+        let deletedCount = 0;
+
+        for (let i = 0; i < uniqueCurrencyPairs.length; i++) {
+            const [base, quota] = uniqueCurrencyPairs[i].split(" ");
+            if (base === quota || base == null) continue;
+            deletedCount += await this.dbService.removeToFavourite({
+                userId,
+                base,
+                quota,
+            });
+        }
+
+        return [200, "Successful", { deleted_count: deletedCount }];
+    }
 }
